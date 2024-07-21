1c2525befe5c03794e0da2f507d799e946567bae
Alberto Limón <119005684+albertolimongithub@users.noreply.github.com>
2024-07-21 18:32:50 +0000

    mi perfil casi terminado

--- src/front/js/component/Navbar/navbar.js
+++ src/front/js/component/Navbar/navbar.js
@@ -6,63 +6,65 @@ import { LuLogOut } from "react-icons/lu";
 
 
 export const Navbar = () => {
-	return (
-		<>
-			<nav className="navbar navbar-expand-lg navbar-light">
-				<div className="container">
-					<div className="navbar-brand">
-						<img className="imagenPerfil" src="https://i.pinimg.com/originals/ec/3e/3d/ec3e3d06ff9aaacd229c7c288308340e.png" />
-						<span className="">My Profile</span>
-					</div>
-					<div className="dropdown">
-						<a className="btn btn-transparent dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-
-						</a>
-
-						<ul className="dropdown-menu">
-							<Link to="/profile">
-								<li><a className="dropdown-item" href="#"><MdOutlineSettings /> Settings</a></li>
-							</Link>
 
 
-							<li><hr className="dropdown-divider" /></li>
-							<li><a className="dropdown-item" href="#"><LuLogOut /> Logout</a></li>
-						</ul>
-					</div>
+return (
+	<>
+		<nav className="navbar navbar-expand-lg navbar-light">
+			<div className="container">
+				<div className="navbar-brand">
+					<img className="imagenPerfil" src="https://i.pinimg.com/originals/ec/3e/3d/ec3e3d06ff9aaacd229c7c288308340e.png" />
+					<span className="">My Profile</span>
+				</div>
+				<div className="dropdown">
+					<a className="btn btn-transparent dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
 
-					<button
-						className="navbar-toggler border-0"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#navbarNavAltMarkup"
-						aria-controls="navbarNavAltMarkup"
-						aria-expanded="false"
-						aria-label="Toggle navigation"
-					>
+					</a>
 
-					</button>
-					<div
-						className="collapse navbar-collapse justify-content-end"
-						id="navbarNavAltMarkup"
-					>
-						<div className="navbar-nav fs-5">
-							<NavLink activeClassName="" to="/main/characters" className="nav-link fs-2">
-								Characters
-							</NavLink>
-							<NavLink to="/main/episodes" className="nav-link fs-2">
-								Episodes
-							</NavLink>
-							<NavLink
-								className="nav-link fs-2"
-								to="/main/locations"
-							>
-								Locations
-							</NavLink>
-						</div>
+					<ul className="dropdown-menu">
+						<Link to="/profile">
+							<li><a className="dropdown-item" href="#"><MdOutlineSettings /> Settings</a></li>
+						</Link>
+
+
+						<li><hr className="dropdown-divider" /></li>
+						<li><a className="dropdown-item" href="#"><LuLogOut /> Logout</a></li>
+					</ul>
+				</div>
+
+				<button
+					className="navbar-toggler border-0"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNavAltMarkup"
+					aria-controls="navbarNavAltMarkup"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+
+				</button>
+				<div
+					className="collapse navbar-collapse justify-content-end"
+					id="navbarNavAltMarkup"
+				>
+					<div className="navbar-nav fs-5">
+						<NavLink activeClassName="" to="/main/characters" className="nav-link fs-2">
+							Characters
+						</NavLink>
+						<NavLink to="/main/episodes" className="nav-link fs-2">
+							Episodes
+						</NavLink>
+						<NavLink
+							className="nav-link fs-2"
+							to="/main/locations"
+						>
+							Locations
+						</NavLink>
 					</div>
 				</div>
-			</nav>
-		</>
+			</div>
+		</nav>
+	</>
 
-	);
+);
 };
--- src/front/js/component/Profile/profile.js
+++ src/front/js/component/Profile/profile.js
@@ -1,11 +1,27 @@
 import React, { useState, useContext } from "react";
 import "../../../styles/profile.css"
 import { useNavigate } from "react-router-dom";
+import { Countries } from "./countries";
 
 export const Profile = () => {
 
+
+    const [inputFirstName, setInputFirstName] = useState("");
+    const [inputLastName, setInputLastName] = useState("");
+    const [inputUsername, setInputUsername] = useState("");
     const [inputEmail, setInputEmail] = useState("");
-    const [inputPassword, setInputPassword] = useState("");
+    const [inputPhone, setInputPhone] = useState("");
+    const [inputBirthDate, setInputBirthDate] = useState("");
+    const [inputCountry, setInputCountry] = useState("");
+    const [inputPostalCode, setInputPostalCode] = useState("");
+
+    console.log(inputFirstName)
+    console.log(inputLastName)
+    console.log(inputUsername)
+    console.log(inputEmail)
+    console.log(inputBirthDate)
+    console.log(inputCountry)
+    console.log(inputPostalCode)
 
     const navigate = useNavigate()
 
@@ -15,36 +31,36 @@ export const Profile = () => {
                 <h1 className="text-dark">
                     My profile
                 </h1>
-                <div className="container d-flex flex-column">
-                    <div class="row d-flex">
-                        <div class="col border d-flex flex-column px-3 py-3 bg-light">
-                            <h3>Personal information</h3>
+                <div className="container d-flex flex-column justify-content-center">
+                    <div className=" d-flex  ">
+                        <div className="col border d-flex flex-column px-3 py-3 bg-light ">
+                            <h3>Account details</h3>
                             <div>
-                                <form className="form-login bg-transparent">
-                                    <button >Edit</button>
+                                <form className="">
+
                                     <div className="row mt-3">
                                         <div className="col form-label-group">
                                             <label className="" htmlFor="inputFirstName">First name</label>
-                                            <input type="text" id="inputEmailLogin" className="form-control w-100" placeholder="" required="" autoFocus=""
-                                                onChange={(event) => setInputEmail(event.target.value)}
-                                                value={inputEmail} />
+                                            <input type="text" id="inputFirstName" className="form-control w-100" placeholder="" required="" autoFocus=""
+                                                onChange={(event) => setInputFirstName(event.target.value)}
+                                                value={inputFirstName} />
 
                                         </div>
 
                                         <div className="col form-label-group">
                                             <label className="" htmlFor="inputLastName">Last name</label>
-                                            <input type="text" id="inputEmailLogin" className="form-control w-100" placeholder="" required="" autoFocus=""
-                                                onChange={(event) => setInputEmail(event.target.value)}
-                                                value={inputEmail} />
+                                            <input type="text" id="inputLastName" className="form-control w-100" placeholder="" required="" autoFocus=""
+                                                onChange={(event) => setInputLastName(event.target.value)}
+                                                value={inputLastName} />
 
                                         </div>
                                     </div>
 
                                     <div className="form-label-group mt-3">
                                         <label className="" htmlFor="inputUsername">Username</label>
-                                        <input type="text" id="inputinputUsername" className="form-control w-75" placeholder="" required="" autoFocus=""
-                                            onChange={(event) => setInputEmail(event.target.value)}
-                                            value={inputEmail} />
+                                        <input type="text" id="inputUsername" className="form-control w-75" placeholder="" required="" autoFocus=""
+                                            onChange={(event) => setInputUsername(event.target.value)}
+                                            value={inputUsername} />
 
                                     </div>
 
@@ -55,46 +71,35 @@ export const Profile = () => {
                                             value={inputEmail} />
 
                                     </div>
-
-                                    <div className="form-label-group mt-4">
-                                        <label className="" htmlFor="inputPhone">Phone</label>
-                                        <input type="text" id="inputPhone" className="form-control w-50" placeholder="" required=""
-                                            onChange={(event) => setInputPassword(event.target.value)}
-                                            value={inputPassword} />
-
-                                    </div>
-
-                                    <div className="form-label-group mt-4">
-                                        <label className="" htmlFor="inputDate">Birth date</label>
-                                        <input type="date" id="inputDate" className="form-control w-50" placeholder=""
-                                            onChange={(event) => setInputPassword(event.target.value)}
-                                            value={inputPassword} />
-
-                                    </div>
-
-                                </form>
-                            </div>
-                        </div>
-
-                        <div class="col border d-flex flex-column px-3 py-3 bg-light">
-                            <h3>Address</h3>
-                            <div className="">
-                                <form className="form-login bg-transparent">
-
-                                    <div className="row mt-3">
-                                        <div className="col form-label-group">
-                                            <label className="" htmlFor="inputFirstName">First name</label>
-                                            <input type="text" id="inputEmailLogin" className="form-control" placeholder="" required="" autoFocus=""
-                                                onChange={(event) => setInputEmail(event.target.value)}
-                                                value={inputEmail} />
+                                    <div className="row">
+                                        <div className="col form-label-group mt-4">
+                                            <label className="" htmlFor="inputPhone">Phone</label>
+                                            <input type="text" id="inputPhone" className="form-control " placeholder="" required=""
+                                                onChange={(event) => setInputPhone(event.target.value)}
+                                                value={inputPhone} />
 
                                         </div>
 
-                                        <div className="col form-label-group">
-                                            <label className="" htmlFor="inputLastName">Last name</label>
-                                            <input type="text" id="inputEmailLogin" className="form-control" placeholder="" required="" autoFocus=""
-                                                onChange={(event) => setInputEmail(event.target.value)}
-                                                value={inputEmail} />
+                                        <div className="col form-label-group mt-4">
+
+                                            <Countries setInputCountry={setInputCountry} />
+                                        </div>
+                                    </div>
+
+                                    <div className="row">
+                                        <div className="col form-label-group mt-4">
+                                            <label className="" htmlFor="inputDate">Birth date</label>
+                                            <input type="date" id="inputDate" className="form-control w-50" placeholder=""
+                                                onChange={(event) => setInputBirthDate(event.target.value)}
+                                                value={inputBirthDate} />
+
+                                        </div>
+
+                                        <div className="col form-label-group mt-4">
+                                            <label className="" htmlFor="inputPostalCode">Postal Code</label>
+                                            <input type="text" id="inputPostalCode" className="form-control" placeholder="" autoFocus=""
+                                                onChange={(event) => setInputPostalCode(event.target.value)}
+                                                value={inputPostalCode} />
 
                                         </div>
                                     </div>
@@ -104,12 +109,19 @@ export const Profile = () => {
                             </div>
                         </div>
 
+
                     </div>
 
-                    <div className="d-flex justify-content-around mt-3">
-                        <button className="btnCancel btn btn-light btn-block boton-submit" type="submit" onClick={() => navigate("/main/characters")}>Cancel</button>
-                        <button className="btnSave btn btn-primary btn-block boton-submit" type="submit" onClick={() => navigate("/main")}>Save changes</button>
+                    <div className="d-flex justify-content-between">
+                        <div className="mt-3 justify-content-around">
+                            <button className="btnCancel btn btn-light btn-block boton-submit" type="submit" onClick={() => navigate("/main/characters")}>Cancel</button>
+                            <button className="btnSave btn btn-primary btn-block boton-submit" type="submit" onClick={() => navigate("/main")}>Save Changes</button>
+                        </div>
+                        <div className="">
+                            <button className="btnDelete btn btn-danger btn-block boton-submit mt-4" type="submit" onClick={() => navigate("/main")}>Delete Account</button>
+                        </div>
                     </div>
+
 
 
                 </div>
